ccbc735d6a37ef778edcdc3be40e630b7a5a394a
Damon Jensen <crazyskip73@gmail.com>
2022-06-20 08:51:01 +1000

    Restyle footer and fix width issue on larger screens

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="text-center font-light py-3">
+      <div className="text-center font-light py-3 bg-gray-900 text-gray-50">
         <span>
           Copyright &copy; 2019. All rights reserved. KJ Contracting Pty Ltd
         </span>
